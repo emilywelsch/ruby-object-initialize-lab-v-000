987e0cf414cbe54431563225bfcdc5e9d51f2fb2
Emily Welsch <emwelsch@gmail.com>
2018-02-17 17:11:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,20 +3,23 @@ class Dog
     @name = name
     @breed = breed
   end
+# To shorten this use the following
+attr.accessor :name
+attr.accessor :breed
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-end
+#   def name=(name)
+#     @name = name
+#   end
+#
+#   def name
+#     @name
+#   end
+#
+#   def breed=(breed)
+#     @breed = breed
+#   end
+#
+#   def breed
+#     @breed
+#   end
+# end
